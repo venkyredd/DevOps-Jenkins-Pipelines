50c2e59eaa284a48d1a66fad8a744755cd420ba6
root <root@LAPTOP-0FERN5QT.>
2025-03-18 06:04:17 +0000

    Initial commit

---
+++ react-app/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Hello, Jenkins!</h1>
+      <p>Your React application is deployed successfully.</p>
+    </div>
+  );
+}
+
+export default App;
